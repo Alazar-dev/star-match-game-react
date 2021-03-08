94bc8cd1f484b159aebe572766373036284a9963
Alazar-dev <mealazartadesse@gmail.com>
2021-03-08 15:02:06 +0300

    distructured stars

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import { useState } from 'react'
 import './App.css';
-
+// STAR MATCH - V2
 const StarMatch = () => {
-  const stars = utils.random(1, 9)
+	const [stars, setStars] = useState(utils.random(1, 9));
   return (
     <div className="game">
       <div className="help">
         Pick 1 or more numbers that sum to the number of stars
       </div>
       <div className="body">
-        <div className="left">      
-          {utils.range(1, stars).map(starId => 
-            <div key={starId} className="star" />
+        <div className="left">
+          {utils.range(1, stars).map(starId =>
+          <div key={starId} className="star" />
           )}
         </div>
         <div className="right">
-          {utils.range(1, 9).map(number => 
+          {utils.range(1, 9).map(number =>
             <button key={number} className="number">{number}</button>
           )}
         </div>
